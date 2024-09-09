72f5af0652e221a04dbe9c69e6a89250bed89790
David Nelson <david.nelson1@uqconnect.edu.au>
2024-09-09 15:29:38 +1000

    Fix bug in location getter methods

--- src/location.py
+++ src/location.py
@@ -1,40 +1,44 @@
 class Location: 
-    def __init__(self, _x, _y, _z):
+    def __init__(self, x = None, y = None, z = None):
         # Parameter handling and assignment
-        all_none = _x is None and _y is None and _z is None
+        all_none = x is None and y is None and z is None
         all_numeric = (
-            isinstance(_x, (int, float)) 
-            and isinstance(_y, (int, float)) 
-            and isinstance(_z, (int, float))
+            isinstance(x, (int, float)) 
+            and isinstance(y, (int, float)) 
+            and isinstance(z, (int, float))
         )
         if not all_none and not all_numeric:
             raise ValueError("""Invalid parameters. x, y and z must either 
                              all be int/float or all be None.""")
-        self.x = _x
-        self.y = _y
-        self.z = _z
+        if all_numeric:
+            x = float(x)
+            y = float(y)
+            z = float(z)
+        self._x = x
+        self._y = y
+        self._z = z
 
-        # Defining getter and setter methods
-        @property
-        def x(self):
-            self._x
+    # Defining getter and setter methods
+    @property
+    def x(self):
+        return self._x
 
-        @x.setter
-        def x(self, value):
-            self._x = value
+    @x.setter
+    def x(self, value):
+        self._x = value
 
-        @property
-        def y(self):
-            self._y
+    @property
+    def y(self):
+        return self._y
 
-        @y.setter
-        def y(self, value):
-            self._y = value
+    @y.setter
+    def y(self, value):
+        self._y = value
 
-        @property
-        def z(self):
-            self._z
+    @property
+    def z(self):
+        return self._z
 
-        @z.setter
-        def z(self, value):
-            self._z = value
+    @z.setter
+    def z(self, value):
+        self._z = value
--- src/node.py
+++ src/node.py
@@ -1,21 +1,38 @@
 from .edge import Edge
+from .location import Location
 from .utility import validate_instance
 
 class Node: 
-    def __init__(self, edge):
+    def __init__(self, edge, location = None):
         validate_instance(edge, Edge)
-        self.edge = edge
+        if location is None: 
+            location = Location(None, None, None)
+        validate_instance(location, Location)
+        self._edge = edge
+        self._location = edge
+        self._mate = None
+    
+    @property
+    def edge(self):
+        return self._edge
+
+    @property
+    def location(self):
+        return self._location
+    
+    @property
+    def mate(self):
+        return self._mate
+    
+    def is_complete(self):
+        
 
 class ConnectorNode(Node):
     def __init__(self, edge): 
         super().__init__(edge)
-        self._mate = None
-
-    @property
-    def mate(self):
-        return self._mate
 
     def mate_with(self, mate):
+        # Only mate with ConnectorNode instances
         validate_instance(mate, ConnectorNode)
         self._mate = mate
         if mate.mate == self:
@@ -31,13 +48,9 @@ class ConnectorNode(Node):
 class CornerNode(Node):
     def __init__(self, edge): 
         super().__init__(edge)
-        self._mate = None
-
-    @property
-    def mate(self):
-        return self._mate
 
     def mate_with(self, mate):
+        # Only mate with CornerNode instances
         validate_instance(mate, CornerNode)
         self._mate = mate
         if mate.mate == self:
