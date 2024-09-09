45dca2de71c2b82ed4731d0ec30ad556316e0db0
David Nelson <david.nelson1@uqconnect.edu.au>
2024-09-09 17:33:09 +1000

    Write CornerNode is_complete method

--- src/node.py
+++ src/node.py
@@ -9,7 +9,7 @@ class Node:
             location = Location(None, None, None)
         validate_instance(location, Location)
         self._edge = edge
-        self._location = edge
+        self._location = location
         self._mate = None
     
     @property
@@ -43,6 +43,8 @@ class ConnectorNode(Node):
         validate_instance(mate, ConnectorNode)
         if self == mate:
             raise ValueError("A ConnectorNode instance cannot mate with itself.")
+        if self._edge == mate.edge:
+            raise ValueError("Nodes have to be on different edges to mate.")
         self._mate = mate
         if mate.mate == self:
             return
@@ -53,6 +55,24 @@ class ConnectorNode(Node):
             mate.mate.mate_with(None)
             mate.mate_with(self)
 
+    def is_complete(self):
+        # Needs to belong to an edge
+        if not isinstance(self._edge, Edge):
+            return False
+        # Needs to have a non-None Location
+        if self._location == Location(None, None, None):
+            return False
+        # Needs to have a mate. 
+        if not isinstance(self._mate, ConnectorNode):
+            return False
+        # That mate cannot be itself. 
+        if self == self.mate:
+            return False
+        # That mate cannot have the same edge as self. 
+        if self.edge == self.mate.edge:
+            return False
+        else:
+            return True
 
 class CornerNode(Node):
     def __init__(self, edge): 
@@ -60,11 +80,18 @@ class CornerNode(Node):
 
     def mate_with(self, mate):
         # Only mate with CornerNode instances
-        validate_instance(mate, CornerNode)
+        if mate is not None and not isinstance(mate, CornerNode) :
+            raise TypeError(f"""Invalid parameter. {mate} must be an instance 
+                            of CornerNode or None.""")
         if self == mate:
             raise ValueError("A CornerNode instance cannot mate with itself.")
+        if mate is not None and self._edge == mate.edge:
+            raise ValueError("""A CornerNode instance cannot mate with another 
+                             CornerNode instance on the same edge.""")
         self._mate = mate
-        if mate.mate == self:
+
+        # Now update the mate's information
+        if mate is None or mate.mate == self:
             return
         elif mate.mate is None:
             mate.mate_with(self)
@@ -91,4 +118,3 @@ class CornerNode(Node):
             return False
         else:
             return True
-
--- tests/test_node.py
+++ tests/test_node.py
@@ -38,6 +38,7 @@ def test_connector_nodes_mate_with_method():
     connector_node_2 = ConnectorNode(edge_2)
     edge_3 = Edge(Panel(Orientation.ROOF))
     connector_node_3 = ConnectorNode(edge_3)
+    connector_node_4 = ConnectorNode(edge)
     assert connector_node.mate == None
     assert connector_node_2.mate == None
 
@@ -45,6 +46,10 @@ def test_connector_nodes_mate_with_method():
     assert connector_node.mate == connector_node_3
     assert connector_node_2.mate == None
     assert connector_node_3.mate == connector_node
+
+    # Nodes must be on different edges to mate.
+    with pytest.raises(ValueError):
+        connector_node.mate_with(connector_node_4)
 
 ###############################################################################
 # CornerNode Tests
@@ -66,6 +71,7 @@ def test_corner_nodes_mate_with_method():
     corner_node_2 = CornerNode(edge_2)
     edge_3 = Edge(Panel(Orientation.ROOF))
     corner_node_3 = CornerNode(edge_3)
+    corner_node_4 = CornerNode(edge)
     assert corner_node.mate == None
     assert corner_node_2.mate == None
 
@@ -74,18 +80,40 @@ def test_corner_nodes_mate_with_method():
     assert corner_node_2.mate == None
     assert corner_node_3.mate == corner_node
 
+    # Nodes must be on different edges to mate.
+    with pytest.raises(ValueError):
+        corner_node.mate_with(corner_node_4)
+
 def test_corner_node_is_complete_method():
     edge = Edge(Panel(Orientation.FLOOR))
     corner_node = CornerNode(edge)
     corner_node_3 = CornerNode(edge)
-    assert corner_node.is_complete() == False
     location = Location(1, 2, 3)
-    corner_node.location = location
-    assert corner_node.is_complete() == False
     edge_2 = Edge(Panel(Orientation.WALL))
     corner_node_2 = CornerNode(edge_2)
+    edge = Edge(Panel(Orientation.FLOOR))
+
+    # Has edge but no location or mate (fails)
+    assert corner_node.is_complete() == False
+
+    # Has edge and location but no mate (fails)
+    corner_node.location = location
+    assert corner_node.is_complete() == False
+
+    # Has edge and location and a legitimate mate (passes)
     corner_node.mate_with(corner_node_2)
     assert corner_node.is_complete() == True
-    edge = Edge(Panel(Orientation.FLOOR))
-    corner_node.mate_with(corner_node_3)
-    assert corner_node.is_complete() == False
+
+    # Has edge and location but mate is illegitimate (shares same edge) (fails)
+    with pytest.raises(ValueError):
+        corner_node.mate_with(corner_node_3)
+
+    # Has edge and location but mate is illegitimate (mates with itself)(fails)
+    with pytest.raises(ValueError):
+        corner_node.mate_with(corner_node)
+    
+    # Has edge and a legitimate mate but no location.
+    corner_node_4 = CornerNode(edge_2)
+    corner_node_4.mate_with(corner_node)
+    print("starting failed assertion")
+    assert corner_node_4.is_complete() == False
